3a7b3005ecbca41b859fef32bf0ccb42e7dfabce
Santo Shakil <shakil.mezbah@gmail.com>
2023-10-22 22:52:51 +0600

    WIP

--- src/main.rs
+++ src/main.rs
@@ -26,36 +26,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn start_peer() -> Result<(), Box<dyn std::error::Error>> {
-    let id_keys = libp2p::identity::Keypair::generate_ed25519();
-    let local_peer_id = libp2p::PeerId::from(id_keys.public());
-    println!("Local peer id: {local_peer_id}");
-
-    let noise_config = noise::Config::new(&id_keys)?;
-    let trns = tcp::tokio::Transport::default()
-        .upgrade(upgrade::Version::V1)
-        .authenticate(noise_config)
-        .multiplex(libp2p::yamux::Config::default())
-        .boxed();
-    let mut swarm = SwarmBuilder::with_tokio_executor(
-        trns,
-        mdns::tokio::Behaviour::new(mdns::Config::default(), local_peer_id)?,
-        local_peer_id,
-    )
-    .build();
-
-    swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
+    let mut swarm = build_swarm()?;
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
     loop {
         let event = swarm.select_next_some().await;
         match event {
-            SwarmEvent::Behaviour(mdns::Event::Discovered(list)) => {
-                for (peer, _) in list {
-                    println!("Discovered peer: {:?}", peer);
+            SwarmEvent::Behaviour(AppBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+                for (peer, addr) in list {
+                    println!("Discovered peer: {} with address {}", peer, addr);
                 }
             }
+            SwarmEvent::ConnectionEstablished { peer_id, .. } => {
+                println!("Connection established with {}", peer_id);
+            }
+            SwarmEvent::NewListenAddr { .. } => {}
             _ => {
-                eprintln!("Unhandled Swarm Event: {:?}", event);
+                eprintln!("\n*** Unhandled Event: {:?} ***\n", event);
             }
         }
     }
+}
+
+fn build_swarm() -> Result<libp2p::Swarm<AppBehaviour>, Box<dyn std::error::Error>> {
+    let id_keys = libp2p::identity::Keypair::generate_ed25519();
+    let local_peer_id = libp2p::PeerId::from(id_keys.public());
+    println!("Local peer id: {local_peer_id}");
+    let trns = tcp::tokio::Transport::default()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(noise::Config::new(&id_keys)?)
+        .multiplex(libp2p::yamux::Config::default())
+        .boxed();
+    let mdns = mdns::tokio::Behaviour::new(mdns::Config::default(), local_peer_id)?;
+    let keep_alive = libp2p::swarm::keep_alive::Behaviour::default();
+    let behaviour = AppBehaviour { mdns, keep_alive };
+    Ok(SwarmBuilder::with_tokio_executor(trns, behaviour, local_peer_id).build())
+}
+
+#[derive(libp2p::swarm::NetworkBehaviour)]
+struct AppBehaviour {
+    keep_alive: libp2p::swarm::keep_alive::Behaviour,
+    mdns: mdns::tokio::Behaviour,
 }
